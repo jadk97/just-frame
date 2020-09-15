000003e58f60b1585e5ad137bb6e1fa03d448e6d
Jad K <jad_dis@hotmail.com>
2020-09-15 06:18:08 -0400

    got frame data rendering properly on Character pages

--- src/CharacterData/CharacterData.js
+++ src/CharacterData/CharacterData.js
@@ -2,14 +2,20 @@ import React from "react";
 import CharacterOverview from "./CharacterOverview";
 import SFVData from "../sfv.json";
 import { useParams } from "react-router-dom";
+import CharacterFrameData from "./CharacterFrameData";
 const CharacterData = (props) => {
   const params = useParams();
   console.log(params.character);
   const currentCharacter = SFVData[params.character];
   console.log(currentCharacter);
+  const frameDataNormalMoves = Object.keys(SFVData[params.character].moves.normal).map((key) => {
+    return { ...SFVData[params.character].moves.normal[key] };
+  });
+  console.log("normal moves", frameDataNormalMoves);
   return (
     <div className="character-data">
       <CharacterOverview charaName={params.character} charaStats={currentCharacter.stats}/>
+      <CharacterFrameData frameData={frameDataNormalMoves} />
     </div>
   )
 }
--- src/CharacterData/CharacterFrameData.js
+++ src/CharacterData/CharacterFrameData.js
@@ -1,10 +1,74 @@
 import React from "react";
+import "./CharacterFrameData.css";
 
 const CharacterFrameData = (props) => {
   return (
-    <div className="character-frame-data">
-    
-    </div>)
+    <table className="frame-data-table">
+      <thead>
+        <tr>
+          <th>Move Name</th>
+          <th>Startup</th>
+          <th>Active</th>
+          <th>Recovery</th>
+          <th>On Hit</th>
+          <th>On Block</th>
+          <th>VT oH</th>
+          <th>VT oB</th>
+          <th>VS oH</th>
+          <th>Damage</th>
+          <th>Stun</th>
+          <th>Attack Lvl</th>
+          <th>Cancel Into</th>
+          <th>KD Adv</th>
+          <th>KDR Adv</th>
+          <th>KDRB Adv</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {props.frameData.map((move) => (
+          <tr>
+            <td>{move.moveName}</td>
+            <td>{move.startup}</td>
+            <td>{move.active}</td>
+            <td>{move.recovery || "~"}</td>
+            <td>{move.onHit || "~"}</td>
+            <td>{move.onBlock || "~"}</td>
+            <td>{move.vtcOnHit || "~"}</td>
+            <td>{move.vtcOnBlock || "~"}</td>
+            {move.vscOnHit ? (
+              <td>{move.vscOnHit}</td>
+            ) : (
+                <td>~</td>
+              )}
+            <td>{move.damage}</td>
+            <td>{move.stun}</td>
+            <td>{move.attackLevel}</td>
+            {move.cancelsTo ? (
+              <td>{move.cancelsTo.join(", ")}</td>
+            ): (
+              <td></td>
+            )}
+          
+            {move.kd ? (
+              <td>{move.kd}</td>
+            ) : (
+                <td>~</td>
+              )}
+            {move.kdr ? (
+              <td>{move.kdr}</td>
+            ) : (
+                <td>~</td>
+              )}
+            {move.kdrb ? (
+              <td>{move.kdrb}</td>
+            ) : (
+                <td>~</td>
+              )}
+          </tr>
+        ))}
+      </tbody>
+    </table>)
 }
 
 export default CharacterFrameData;
